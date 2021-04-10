bcd24f64da7e5c29d0d0a8600a2cdc87a18133ba
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-10 19:12:22 +0530

    Player chooses an option

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,16 +1,23 @@
 import java.util.*;
 	public class TicTacToe {
 
-    //creating board
-    public static char[] createBoard() {
-        char[] board = new char[10];
-        for (int i = 1; i < board.length; i++) {
-            System.out.println(board[i]);
-        }
-        return board;
-	}
+    //choosing options
+	public static void chooseOption() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Select X or O:");
+        String userChoice=input.next();
+        System.out.println("User choice is:"+userChoice);
+        
+        if (userChoice.equalsIgnoreCase("x"))
+            System.out.println("Computer's choice:O");
+        else if (userChoice.equalsIgnoreCase("o"))
+            System.out.println("Computer's choice:X");
+        else
+            System.out.println("Enter the correct option!");
+    }
+
         public static void main(String args[]) {
-            createBoard();
+            chooseOption();
         }
 
     }
