90d7c5d555677f260170bfdc074d9efd5061cc2a
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-11 20:33:35 +0530

    Move desired location in the board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,51 +1,95 @@
 import java.util.*;
-
-public class TicTacToe {
-
-    //creating board
-    public static char[] createBoard() {
-        char[] board = new char[10];
-        for (int i = 1; i < board.length; i++) {
-            System.out.println(board[i]);
-        }
-        showBoard(board);
-        return board;
-        }
+import java.awt.*; 
 
 
-    //showing the board
-    public static void showBoard(char[] board){
+public class TicTacToe
+{
+	public char board[] = new char[10] ;
+	public String user_value;
+	
 
+
+	public void createBoard()
+	{
+		for(int i=1; i<10 ;i++)
+		{
+			board[i]= ' ';
+			System.out.println("board:["+i+"]="+board[i]);
+		}
+
+
+	}
+
+	public void chooseOption()
+	{
+		
+		System.out.println("Enter X or O ");
+		Scanner sc_object = new Scanner(System.in);
+		user_value = sc_object.next();
+		
+		if(user_value.equals("x") || user_value.equals("X"))
+		{
+			System.out.println(" computer chooses  'O' ");
+			
+		}
+		else if(user_value.equals("o") || user_value.equals("O"))
+		{
+			System.out.println(" computer chooses 'X' ");
+		}
+		else
+		{
+			System.out.println("invalid input");
+			chooseOption();
+		}
+			
+		
+	}
+	
+	public void showBoard()
+	{
+		
+		
         System.out.println("|---|---|---|");
-        System.out.println("| " + board[0] + "| " + board[1] + "| " + board[2] + "|");
+        System.out.println("| " + board[1] + " | "
+                           + board[2] + " | " + board[3]
+                           + " |");
         System.out.println("|-----------|");
-        System.out.println("| " + board[3] + "| " + board[4] + "| " + board[5] + "|");
+        System.out.println("| " + board[4] + " | "
+                           + board[5] + " | " + board[6]
+                           + " |");
         System.out.println("|-----------|");
-        System.out.println("| " + board[6] + "| " + board[7] + "| " + board[8] + "|");
+        System.out.println("| " + board[7] + " | "
+                           + board[8] + " | " + board[9]
+                           + " |");
         System.out.println("|---|---|---|");
-            }
+    
+	}
+	
+	public void selectLocation()
+	{
+		System.out.println("Choose location between 1 to 9 ");
+		
+		Scanner select = new Scanner(System.in);
+		int choose = select.nextInt();
+		if(board[choose] == ' ')
+		{
+			System.out.println("Location is empty, enter position");
+			
+		}
+		else
+		{
+			System.out.println("Location "+choose+ " is not empty. Choose another location");
+		}
+		}
 
-    //choosing options
-    public static void chooseOption() {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Select X or O:");
-        String userChoice=input.next();
-        System.out.println("User's choice:"+userChoice);
-
-        if (userChoice.equalsIgnoreCase("x"))
-            System.out.println("Computer's choice:O");
-        else if (userChoice.equalsIgnoreCase("o"))
-            System.out.println("Computer's choice:X");
-        else
-            System.out.println("Enter the correct option!");
-    }
-
-    public static void main(String args[])
-    {
-        createBoard();
-        chooseOption();
-
-    }
-
+	public static void main(String[] args)
+	{
+		TicTacToe Game_object = new TicTacToe();
+		Game_object.createBoard();
+		Game_object.chooseOption();
+		Game_object.showBoard();
+		Game_object.selectLocation();
+		
+	}
 }
 
